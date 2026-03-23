54d5906235f52d16a85ff3af03019268b4d88647
SamHsu <yxu166@jh.edu>
2026-03-22 21:37:33 -0400

    refactor: Remove in-memory ID mappings and streamline property handling in Neo4j storage implementations

--- modules/impl-neo4j/src/main/kotlin/edu/jhu/cobra/commons/graph/utils/Neo4JUtils.kt
+++ modules/impl-neo4j/src/main/kotlin/edu/jhu/cobra/commons/graph/utils/Neo4JUtils.kt
@@ -2,13 +2,10 @@ package edu.jhu.cobra.commons.graph.utils
 
 import edu.jhu.cobra.commons.graph.InvalidPropNameException
 import edu.jhu.cobra.commons.value.IValue
-import edu.jhu.cobra.commons.value.StrVal
 import edu.jhu.cobra.commons.value.serializer.DftByteArraySerializerImpl
 import org.neo4j.graphdb.Entity
-import org.neo4j.graphdb.Node as Neo4jNode
-import org.neo4j.graphdb.Relationship as Neo4jEdge
 
-private const val META_ID: String = "__meta_id__"
+private val RESERVED_PROPS = setOf("__meta_id__", "__sid__", "__tag__")
 
 private fun Entity.setProp(
     name: String,
@@ -20,20 +17,12 @@ private inline fun <reified T : IValue> Entity.getProp(byName: String): T? =
 
 val Entity.keys: Collection<String>
     get() =
-        this.propertyKeys.filter { it != META_ID }.distinct()
-
-var Neo4jNode.storageID: String
-    get() = getProp<StrVal>(META_ID)!!.core
-    set(value) = setProp(META_ID, StrVal(value))
-
-var Neo4jEdge.storageID: String
-    get() = getProp<StrVal>(META_ID)!!.core
-    set(value) = setProp(META_ID, StrVal(value))
+        this.propertyKeys.filter { it !in RESERVED_PROPS }.distinct()
 
 operator fun Entity.set(
     byName: String,
     newVal: IValue,
-) = if (byName == META_ID) throw InvalidPropNameException(byName, null) else this.setProp(byName, newVal)
+) = if (byName in RESERVED_PROPS) throw InvalidPropNameException(byName, null) else this.setProp(byName, newVal)
 
 operator fun Entity.get(byName: String): IValue? =
-    if (byName == META_ID) throw InvalidPropNameException(byName, null) else getProp(byName)
+    if (byName in RESERVED_PROPS) throw InvalidPropNameException(byName, null) else getProp(byName)
