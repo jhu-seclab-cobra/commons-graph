7317d14dd20eb6b866c86748074401d202e598db
samhsu-dev <35974430+samhsu-dev@users.noreply.github.com>
2026-03-24 12:47:22 -0400

    ref: reorganize the project source directory to align with industry standards.

--- commons-graph-impl-jgrapht/build.gradle.kts
+++ commons-graph-impl-jgrapht/build.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
-    implementation(project(":graph"))
+    implementation(project(":jhu-seclab-cobra-commons-graph"))
     implementation(libs.cobra.commons.value)
     implementation(libs.jgrapht.core)
     implementation(libs.jgrapht.io)
